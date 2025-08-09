31a89ac509eb86193c123807eb3ac6ff2a8af7ee
Tsegaw-Mao <tsegawjohnj@gmail.com>
2025-08-09 17:22:17 +0300

    task 3

--- alx-movie-app/components/commons/MovieCard.tsx
+++ alx-movie-app/components/commons/MovieCard.tsx
@@ -1,12 +1,19 @@
-const MovieCard: React.FC<{ movie: { title: string, posterUrl: string, description: string } }> = ({ movie }) => {
+import { MovieProps } from "@/interfaces"
+import Image from "next/image"
+
+const MovieCard: React.FC<MovieProps> = ({ title, posterImage, releaseYear }) => {
   return (
-    <div className="border rounded-lg shadow-lg overflow-hidden hover:shadow-xl transition-shadow duration-300">
-      <img src={movie.posterUrl} alt={movie.title} className="w-full h-64 object-cover" />
-      <div className="p-4">
-        <h2 className="text-xl font-bold mb-2">{movie.title}</h2>
-        <p className="text-gray-700 mb-4">{movie.description}</p>
+    <div className="h-[563px]">
+      <div>
+        <Image className="h-[430px] w-full rounded-md hover:cursor-pointer" src={posterImage} width={100} height={100} alt={title} />
+
+      </div>
+      <div className="flex justify-between py-4">
+        <p className="text-xl font-bold">{title}</p>
+        <p className="text-xl text-[#E2D609]">{releaseYear}</p>
       </div>
     </div>
   )
 }
-export default MovieCard;
+
+export default MovieCard
